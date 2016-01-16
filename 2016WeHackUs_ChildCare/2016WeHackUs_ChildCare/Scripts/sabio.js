b89f6b9b5a4b3b3e8dfea601aea792b295f034df
neil0609 <neilengracia@yahoo.com>
2016-01-16 11:05:52 -0800

    angular js, sabio.js and theme

---
+++ 2016WeHackUs_ChildCare/2016WeHackUs_ChildCare/Scripts/sabio.js
@@ -0,0 +1,23 @@
+﻿var sabio = {
+    layout: {},
+    page: {
+        handlers: {},
+        startUp: null
+    },
+    services: {},
+    ui: {
+        notifications: {},
+        startUp: null
+    }
+};
+
+
+sabio.layout.startUp = function () {
+
+    //this does a null check on sabio.page.startUp
+    if (sabio.page.startUp) {
+        console.log("sabio.page.startup");
+        sabio.page.startUp();
+    }
+};
+$(document).ready(sabio.layout.startUp);
