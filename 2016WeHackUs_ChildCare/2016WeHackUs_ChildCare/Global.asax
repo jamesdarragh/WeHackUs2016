bd5bbcbc6bb9e89ad3a542813072c0ce22068ecc
jamesdarragh <jimdarragh@hotmail.com>
2016-01-15 20:49:45 -0800

    Created Starter File

---
+++ 2016WeHackUs_ChildCare/2016WeHackUs_ChildCare/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2016WeHackUs_ChildCare.MvcApplication" Language="C#" %>
---
+++ 2016WeHackUs_ChildCare/2016WeHackUs_ChildCare/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2016WeHackUs_ChildCare.Startup))]
+namespace _2016WeHackUs_ChildCare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
